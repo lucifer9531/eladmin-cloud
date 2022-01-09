1444b2fcfa3eac039300cef53bb78e88d2bea3e1
baixp <baixp@dustess.com>
2022-01-09 21:18:15 +0800

    feat: Added WebConfiguration

---
+++ eladmin-core/eladmin-starter-web/src/main/java/com/google/core/web/config/WebConfiguration.java
@@ -0,0 +1,50 @@
+package com.google.core.web.config;
+
+import com.google.core.redis.core.RedisService;
+import com.google.core.web.listener.RequestMappingScanListener;
+import com.google.core.web.support.LoginUserArgumentResolver;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * WEB配置类
+ * @author iris
+ */
+@Slf4j
+@Configuration
+@AllArgsConstructor
+public class WebConfiguration implements WebMvcConfigurer {
+
+	private final RedisService redisService;
+
+	/**
+	 * Token参数解析
+	 *
+	 * @param argumentResolvers 解析类
+	 */
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		//注入用户信息
+		argumentResolvers.add(new LoginUserArgumentResolver());
+	}
+
+	/**
+	 * 资源扫描监听器类
+	 *
+	 * @return RequestMappingScanListener
+	 */
+	@Bean
+	@ConditionalOnMissingBean(RequestMappingScanListener.class)
+	public RequestMappingScanListener resourceAnnotationScan() {
+		RequestMappingScanListener scan = new RequestMappingScanListener(redisService);
+		log.info("资源扫描类.[{}]", scan);
+		return scan;
+	}
+}
